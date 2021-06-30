9787c7dc5ba333450364caa183b7e2506b103689
wrajwan-discovery <william_rajwan@discovery.com>
2021-06-29 20:02:10 -0400

    updated logic to accound to missing assets, added firebase

--- src/js/components/posterLogo.js
+++ src/js/components/posterLogo.js
@@ -1,13 +1,19 @@
 $.getJSON("assets/data/data.json", function(items) {
     for(var item = 0; item < 28; item++) {
-        $('#grid').append(
-        "<a class='grid-item show' href='" + items[item].posterWithLogoImage + "' target='_blank' download>" +
-              "<img src='" + items[item].posterWithLogoImage + '?w=800&f=JPG&p=true&q=60' + "' />" +
-              "<div class='grid-item-meta-wrapper'>" +
-                "<div class='grid-item-series-title'>" + items[item].displayName + "</div>" +
-                "<div class='grid-item-network'>" + items[item].channelName + "</div>" +
-              "</div>" +
-        "</a>");
+      if (items[item].posterWithLogoImage.length > 0) {
+          $('#grid').append(
+          "<a class='grid-item show' href='" + items[item].posterWithLogoImage + "' target='_blank' download>" +
+                "<img src='" + items[item].posterWithLogoImage + '?w=800&f=JPG&p=true&q=60' + "' />" +
+                "<div class='grid-item-meta-wrapper'>" +
+                  "<div class='grid-item-series-title'>" + items[item].displayName + "</div>" +
+                  "<div class='grid-item-network'>" + items[item].channelName + "</div>" +
+                "</div>" +
+          "</a>");
+        } else {
+          $('#grid').append(
+            "<div class='grid-item' style='display: none'></div>"
+          );
+        }
     }
   });
   
@@ -19,20 +25,25 @@ $.getJSON("assets/data/data.json", function(items) {
     if (searchField === "") {
         if($(window).scrollTop()==($(document).height()-window.innerHeight)){
             $.getJSON("assets/data/data.json", function(items) {
-              for(var item = itemPosition; item < itemNext; item++) {
-                  $('#grid').append(
-                    "<a class='grid-item show' href='" + items[item].posterWithLogoImage + "' target='_blank' download>" +
-                      "<img src='" + items[item].posterWithLogoImage + '?w=800&f=JPG&p=true&q=60' + "' />" +
-                      "<div class='grid-item-meta-wrapper'>" +
-                        "<div class='grid-item-series-title'>" + items[item].displayName + "</div>" +
-                        "<div class='grid-item-network'>" + items[item].channelName + "</div>" +
-                      "</div>" +
-                  "</a>");
-              }
+                for(var item = itemPosition; item < itemNext; item++) {
+                  if (items[item].posterWithLogoImage.length > 0) {
+                    $('#grid').append(
+                      "<a class='grid-item show' href='" + items[item].posterWithLogoImage + "' target='_blank' download>" +
+                        "<img src='" + items[item].posterWithLogoImage + '?w=800&f=JPG&p=true&q=60' + "' />" +
+                        "<div class='grid-item-meta-wrapper'>" +
+                          "<div class='grid-item-series-title'>" + items[item].displayName + "</div>" +
+                          "<div class='grid-item-network'>" + items[item].channelName + "</div>" +
+                        "</div>" +
+                    "</a>");
+                  } else {
+                    $('#grid').append(
+                      "<div class='grid-item' style='display: none'></div>"
+                    );
+                  }
+                }
             });
-          } else {}
-        } else {
-        }
+          }
+        } 
   });
   
   function delay(callback, ms) {
@@ -55,19 +66,25 @@ $.getJSON("assets/data/data.json", function(items) {
       $.getJSON('assets/data/data.json', function(items) {
         if (searchField === "") {
         for(var item = 0; item < 28; item++) {
-          $('#grid').append(
-          "<a class='grid-item show' href='" + items[item].posterWithLogoImage + "' target='_blank' download>" +
-                "<img src='" + items[item].posterWithLogoImage + '?w=800&f=JPG&p=true&q=60' + "' />" +
-                "<div class='grid-item-meta-wrapper'>" +
-                  "<div class='grid-item-series-title'>" + items[item].displayName + "</div>" +
-                  "<div class='grid-item-network'>" + items[item].channelName + "</div>" +
-                "</div>" +
-          "</a>");
-        }
+          if (items[item].posterWithLogoImage.length > 0) {
+            $('#grid').append(
+              "<a class='grid-item show' href='" + items[item].posterWithLogoImage + "' target='_blank' download>" +
+                    "<img src='" + items[item].posterWithLogoImage + '?w=800&f=JPG&p=true&q=60' + "' />" +
+                    "<div class='grid-item-meta-wrapper'>" +
+                      "<div class='grid-item-series-title'>" + items[item].displayName + "</div>" +
+                      "<div class='grid-item-network'>" + items[item].channelName + "</div>" +
+                    "</div>" +
+              "</a>");
+            } else {
+              $('#grid').append(
+                "<div class='grid-item' style='display: none'></div>"
+              );
+            }
+          }
         } else {
             $.each(items, function(key, item){
-            if (item.displayName.search(expression) != -1)
-              {
+            if (item.displayName.search(expression) != -1) {
+              if (item.posterWithLogoImage.length > 0) {
                   $('#grid').append(
                     "<a class='grid-item show' href='" + item.posterWithLogoImage + "' target='_blank' download>" +
                         "<img src='" + item.posterWithLogoImage + '?w=800&f=JPG&p=true&q=60' + "' />" +
@@ -76,7 +93,12 @@ $.getJSON("assets/data/data.json", function(items) {
                           "<div class='grid-item-network'>" + item.channelName + "</div>" +
                         "</div>" +
                     "</a>");
-                } 
+                } else {
+                  $('#grid').append(
+                    "<div class='grid-item' style='display: none'></div>"
+                  );
+                }
+              }
           });
         }   
       });
--- src/js/components/square.js
+++ src/js/components/square.js
@@ -1,5 +1,6 @@
 $.getJSON("assets/data/data.json", function(items) {
     for(var item = 0; item < 28; item++) {
+      if (items[item].coverArtworkImage.length > 0) {
         $('#grid').append(
         "<a class='grid-item show' href='" + items[item].coverArtworkImage + "' target='_blank' download>" +
               "<img src='" + items[item].coverArtworkImage + '?w=800&f=JPG&p=true&q=60' + "' />" +
@@ -8,6 +9,11 @@ $.getJSON("assets/data/data.json", function(items) {
                 "<div class='grid-item-network'>" + items[item].channelName + "</div>" +
               "</div>" +
         "</a>");
+      } else {
+        $('#grid').append(
+          "<div class='grid-item' style='display: none'></div>"
+        );
+      }
     }
   });
   
@@ -20,6 +26,7 @@ $.getJSON("assets/data/data.json", function(items) {
         if($(window).scrollTop()==($(document).height()-window.innerHeight)){
             $.getJSON("assets/data/data.json", function(items) {
               for(var item = itemPosition; item < itemNext; item++) {
+                if (items[item].coverArtworkImage.length > 0) {
                   $('#grid').append(
                     "<a class='grid-item show' href='" + items[item].coverArtworkImage + "' target='_blank' download>" +
                       "<img src='" + items[item].coverArtworkImage + '?w=800&f=JPG&p=true&q=60' + "' />" +
@@ -28,6 +35,11 @@ $.getJSON("assets/data/data.json", function(items) {
                         "<div class='grid-item-network'>" + items[item].channelName + "</div>" +
                       "</div>" +
                   "</a>");
+                } else {
+                  $('#grid').append(
+                    "<div class='grid-item' style='display: none'></div>"
+                  );
+                }
               }
             });
           } else {}
@@ -56,20 +68,26 @@ $.getJSON("assets/data/data.json", function(items) {
       $.getJSON('assets/data/data.json', function(items) {
         if (searchField === "") {
         for(var item = 0; item < 28; item++) {
-          $('#grid').append(
-          "<a class='grid-item show' href='" + items[item].coverArtworkImage + "' target='_blank' download>" +
-                "<img src='" + items[item].coverArtworkImage + '?w=800&f=JPG&p=true&q=60' + "' />" +
-                "<div class='grid-item-meta-wrapper'>" +
-                  "<div class='grid-item-series-title'>" + items[item].displayName + "</div>" +
-                  "<div class='grid-item-network'>" + items[item].channelName + "</div>" +
-                "</div>" +
-          "</a>");
+          if (items[item].coverArtworkImage.length > 0) {
+            $('#grid').append(
+            "<a class='grid-item show' href='" + items[item].coverArtworkImage + "' target='_blank' download>" +
+                  "<img src='" + items[item].coverArtworkImage + '?w=800&f=JPG&p=true&q=60' + "' />" +
+                  "<div class='grid-item-meta-wrapper'>" +
+                    "<div class='grid-item-series-title'>" + items[item].displayName + "</div>" +
+                    "<div class='grid-item-network'>" + items[item].channelName + "</div>" +
+                  "</div>" +
+            "</a>");
+          }else {
+            $('#grid').append(
+              "<div class='grid-item' style='display: none'></div>"
+            );
+          }
         }
         } else {
   
             $.each(items, function(key, item){
-            if (item.displayName.search(expression) != -1)
-              {
+            if (item.displayName.search(expression) != -1) {
+              if (item.coverArtworkImage.length > 0) {
                   $('#grid').append(
                     "<a class='grid-item show' href='" + item.coverArtworkImage + "' target='_blank' download>" +
                         "<img src='" + item.coverArtworkImage + '?w=800&f=JPG&p=true&q=60' + "' />" +
@@ -78,6 +96,11 @@ $.getJSON("assets/data/data.json", function(items) {
                           "<div class='grid-item-network'>" + item.channelName + "</div>" +
                         "</div>" +
                     "</a>");
+                  } else {
+                    $('#grid').append(
+                      "<div class='grid-item' style='display: none'></div>"
+                    );
+                  } 
                 } 
           });
         }   
